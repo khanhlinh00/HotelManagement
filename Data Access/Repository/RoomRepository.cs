574043a18130f9704e0f3413eb2ba31a144043c1
khanhlinh00 <65752339+khanhlinh00@users.noreply.github.com>
2022-03-12 21:50:32 +0700

    roomDAO+repo

---
+++ Data Access/Repository/RoomRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject;
+using Data_Access;
+
+namespace Data_Access.Repository
+{
+    public class RoomRepository : IRoomRepository
+    {
+        public RoomObject GetRoomByID(int roomID) => RoomDAO.Instance.GetRoomByID(roomID);
+        public IEnumerable<RoomObject> GetRooms() => RoomDAO.Instance.GetRoomsList();
+        public void InsertRoom(RoomObject room) => RoomDAO.Instance.AddNew(room);
+
+        public void DeleteRoom(int roomID)=> RoomDAO.Instance.Remove(roomID);
+
+        public void UpdateRoom(RoomObject room)=> RoomDAO.Instance.Update(room);
+    }
+}
